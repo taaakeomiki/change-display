a54273409eba0754b66d2c2112b982c745903b42
taaakeomiki <ee41236@meiji.ac.jp>
2017-04-19 19:13:23 +0900

    change display
    Display change red or green.

--- MySecondApp/ViewController.swift
+++ MySecondApp/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func unwindToTop(segue: UIStoryboardSegue){
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
